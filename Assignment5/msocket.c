a80988564c5de81ad5fc1014dcbbe8c3f82c3af5
ishaaniitkgp <ishaan.vmcsje@gmail.com>
2024-03-12 23:27:02 +0530

    msocket.h and msocket.c

--- Assignment5/msocket.c
+++ Assignment5/msocket.c
@@ -1,1 +1,29 @@
+#include "msocket.h"
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int m_socket(int domain, int type, int protocol) {
+    if(type != SOCK_MTP) {
+        fprintf(stderr, "Invalid socket type\n");
+        exit(1);
+    }
+    int sock_id = socket(domain, SOCK_DGRAM, protocol);
+    if (sock_id < 0) {
+        perror("socket");
+        exit(1);
+    }
+    return sock_id;
+}
+
+void mbind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
+    if (bind(sockfd, addr, addrlen) < 0) {
+        perror("bind");
+        exit(1);
+    }
+}
 
--- Assignment5/msocket.h
+++ Assignment5/msocket.h
@@ -1,1 +1,48 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define SOCK_MTP 3
+
+struct SOCK_INFO {
+    int sock_id = 0;
+    int errno = 0;
+    int port = 0;
+    char *ip = NULL;
+};
+
+typedef struct swnd {
+        int seq_number[5];
+        int window_size;
+} swnd;
+
+typedef struct rwnd {
+        int seq_number[5];
+        int window_size;
+} rwnd;
+
+struct Socket {
+    int free = 0;
+    int pid;
+    int sock_id;
+    char *destip;
+    int destport;
+    char *sendbuf[10];
+    char *recvbuf[5];  
+    swnd sender_window;
+    rwnd receiver_window;
+};
+
+int errno;
+
+//A shared memory chunk SM containing the information about 25 MTP sockets
+struct SOCK_INFO SM[25];
+
+int m_socket(int domain, int type, int protocol);
+int m_bind(int sock_id, char *srcip, int srcport, char *destip, int destport);
+int m_sendto(int sock_id, char *buf, size_t len, char *destip, int destport);
+int m_recvfrom(int sock_id, char *buf, size_t len, char *srcip, int srcport);
+int m_close(int sock_id);
 
